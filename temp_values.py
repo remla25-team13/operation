5178e1cf6e9a6decea088a366934d42a103f7102
“Tijmen <tgmeijer@student.tudelft.nl>
2025-05-19 13:23:39 +0200

    add monitoring test

---
+++ temp_values.py
@@ -0,0 +1,16 @@
+from prometheus_client import start_http_server, Counter
+import time
+
+# Define the metric
+total_reviews = Counter('total_reviews', 'Total number of reviews processed')
+
+def process_review():
+    # Your logic to process a review
+    total_reviews.inc()  # Increment the counter by 1
+
+if __name__ == '__main__':
+    # Start the Prometheus metrics server on port 8000
+    start_http_server(9090)
+    while True:
+        process_review()
+        time.sleep(1)
